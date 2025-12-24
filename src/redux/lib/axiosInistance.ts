271dd8cd5081ea1da3fc88b613bc12310d04ee1a
niloytalukde <niloytalukderme@gmail.com>
2025-12-24 23:39:01 +0530

    Added About page

--- src/redux/lib/axiosInistance.ts
+++ src/redux/lib/axiosInistance.ts
@@ -1,6 +1,36 @@
+import axios from "axios";
 
-import axios from 'axios';
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.BASE_URL,
-  timeout: 1000,
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 10000,
+  withCredentials: true, 
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response.data; // optional but clean
+  },
+  (error) => {
+    const status = error?.response?.status;
+
+   
+    if (status === 401) {
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error?.response?.data || error);
+  }
+);
