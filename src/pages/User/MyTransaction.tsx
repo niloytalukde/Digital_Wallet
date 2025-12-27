7c168a7c2c23236c821fcc2409280ff8581fc702
niloytalukde <niloytalukderme@gmail.com>
2025-12-27 20:07:07 +0530

    Added My transation agent and user

--- src/pages/User/MyTransaction.tsx
+++ src/pages/User/MyTransaction.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useMyTransactionQuery } from "@/redux/features/transaction/transaction.api";
 
 
@@ -7,8 +8,38 @@ const MyTransaction = () => {
     const {data}=useMyTransactionQuery(undefined);
     console.log(data);
     return (
-        <div>
-            
+        <div className="space-y-4 p-4">
+            <h1 className="text-3xl flex justify-center">My Transactions</h1>
+
+            {data?.map((transaction:any)=>(
+                  <div className="flex items-center justify-between bg-white shadow-md rounded-xl p-4 border">
+      
+      {/* Left - From */}
+      <div className="flex-1">
+        <p className="text-xs text-gray-500">From</p>
+        <p className="font-medium text-gray-800">
+          {transaction.from?.ownerEmail}
+        </p>
+      </div>
+
+      {/* Center - Amount */}
+      <div className="flex-1 text-center">
+        <p className="text-xs text-gray-500">Amount</p>
+        <p className="text-lg font-bold text-green-600">
+          ৳ {transaction.amount}
+        </p>
+      </div>
+
+      {/* Right - To */}
+      <div className="flex-1 text-right">
+        <p className="text-xs text-gray-500">To</p>
+        <p className="font-medium text-gray-800">
+          {transaction.to?.ownerEmail}
+        </p>
+      </div>
+
+    </div>
+            ))}
         </div>
     );
 };
